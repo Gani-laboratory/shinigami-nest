4f6271ff391f7b70e02cdb06533eec6b45e79e2c
QUERY-GANI <ahmadxgani01@gmail.com>
2021-06-26 07:41:50 +0700

    experiment roles and permission

---
+++ src/modules/auth/auth.guard.ts
@@ -0,0 +1,14 @@
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    console.log('my auth guard');
+    const req = context.switchToHttp().getRequest();
+    console.log(req.user);
+    return true;
+  }
+}
